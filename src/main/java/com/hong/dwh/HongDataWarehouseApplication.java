455b7f3a48d492eee241cef536b9beedbda73945
Lee, Powei <leepw1221@gmail.com>
2023-01-24 18:18:14 +0800

    Initial commit

---
+++ src/main/java/com/hong/dwh/HongDataWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.hong.dwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongDataWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongDataWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hong/dwh/HongDataWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.dwh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongDataWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
