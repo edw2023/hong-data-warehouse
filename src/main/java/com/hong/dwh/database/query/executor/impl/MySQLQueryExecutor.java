60b0be83301cbc8b046f1e8284d57651d8ea24ae
Lee, Powei <leepw1221@gmail.com>
2023-01-26 15:23:55 +0800

    update MySQLQueryExecutor.java

--- src/main/java/com/hong/dwh/database/query/executor/impl/MySQLQueryExecutor.java
+++ src/main/java/com/hong/dwh/database/query/executor/impl/MySQLQueryExecutor.java
@@ -3,10 +3,18 @@ package com.hong.dwh.database.query.executor.impl;
 import com.hong.dwh.database.dto.ApiContextDto;
 import com.hong.dwh.database.query.executor.QueryExecutor;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class MySQLQueryExecutor implements QueryExecutor {
 
     @Override
     public Object execute(ApiContextDto context) {
+        String sql = context.getSql();
+        Connection connection = null;
+        Statement pstmt = null;
+
         return null;
+
     }
 }
