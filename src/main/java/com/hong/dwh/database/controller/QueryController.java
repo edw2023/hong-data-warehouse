7ba218a8b5e39635a1ad9e41d95ebe0c27dd3cfd
Lee, Powei <leepw1221@gmail.com>
2023-01-26 15:04:00 +0800

    add QueryController.java

---
+++ src/main/java/com/hong/dwh/database/controller/QueryController.java
@@ -0,0 +1,17 @@
+package com.hong.dwh.database.controller;
+
+import com.hong.dwh.database.dto.ApiContextDto;
+import com.hong.dwh.database.service.QueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class QueryController {
+
+    @Autowired
+    private QueryService queryService;
+
+    public void queryFromDB(ApiContextDto context){
+        queryService.executeSQL(context);
+    }
+}
