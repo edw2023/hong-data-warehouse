abe4b1b2e740b718bb7b51cf0ed675de12bd6731
Lee, Powei <leepw1221@gmail.com>
2023-01-26 16:18:00 +0800

    update connector classes

--- src/main/java/com/hong/dwh/database/query/connector/impl/MySQLConnector.java
+++ src/main/java/com/hong/dwh/database/query/connector/impl/MySQLConnector.java
@@ -1,7 +1,23 @@
 package com.hong.dwh.database.query.connector.impl;
 
+import com.hong.dwh.database.dto.ApiDto;
 import com.hong.dwh.database.query.connector.DatabaseConnector;
+import org.apache.hadoop.hbase.client.Connection;
 
 public class MySQLConnector implements DatabaseConnector {
 
+    @Override
+    public Connection createNewConnection(ApiDto context) {
+        return null;
+    }
+
+    @Override
+    public Connection getConnection(ApiDto context) {
+        return null;
+    }
+
+    @Override
+    public void closeConnection() {
+
+    }
 }
