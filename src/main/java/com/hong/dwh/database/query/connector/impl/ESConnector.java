0913bd3c8e9e53d53a09663e6a53e694910a5cc3
Lee, Powei <leepw1221@gmail.com>
2023-01-27 15:45:08 +0800

    add ESConnector

--- src/main/java/com/hong/dwh/database/query/connector/impl/ESConnector.java
+++ src/main/java/com/hong/dwh/database/query/connector/impl/ESConnector.java
@@ -1,11 +1,20 @@
 package com.hong.dwh.database.query.connector.impl;
 
+import co.elastic.clients.elasticsearch.graph.Connection;
 import com.hong.dwh.database.common.BeanFactory;
 import com.hong.dwh.database.query.connector.DatabaseConnector;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ESConnector implements DatabaseConnector {
 
     private static ESConnector instance = null;
+    Long renewalTime = 30*1000*1000L;
+    Long closeIntervalTime = 60*1000*1000L;
+    private Map<String,Map<Long, RestHighLevelClient>> clientPool = new HashMap<>();
+    private Map<Long,RestHighLevelClient> disconnectedClients = new HashMap<>();
 
     private ESConnector() throws Exception {
         throw new Exception("Not allowed to instantiate");
